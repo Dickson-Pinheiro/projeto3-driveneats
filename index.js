5cbe027c103a59ace5199e4474fa4271f4e3e9cf
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2022-10-12 23:06:20 -0300

    modal e link pra o whatsapp configurado

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 const bebidas = document.querySelectorAll("input[name='bebidas']");
 const pratos = document.querySelectorAll("input[name='pratos']");
 const sobremesas = document.querySelectorAll("input[name='sobremesas']");
-const itemsSelected = []
+const categoriesSelected = {
+    prato: "",
+    bebida: "",
+    sobremesa: ""
+}
+
+const menu = {
+    pratos: {
+        prato1: {
+            name: "Frango Yin Yang",
+            price: 14.90
+        },
+        prato2: {
+            name: "Frango Yin Yang",
+            price: 14.90
+        },
+        prato3: {
+            name: "Frango Yin Yang",
+            price: 14.90
+        }
+    },
+    bebidas: {
+        bebida1: {
+            name: "Coquinha gelada",
+            price: 4.90
+        },
+        bebida2: {
+            name: "Coquinha gelada",
+            price: 4.90
+        },
+        bebida3: {
+            name: "Coquinha gelada",
+            price: 4.90
+        }
+    },
+    sobremesas: {
+        sobremesa1: {
+            name: "Pudim",
+            price: 7.90
+        },
+        sobremesa2: {
+            name: "Pudim",
+            price: 7.90
+        },
+        sobremesa3: {
+            name: "Pudim",
+            price: 7.90
+        }
+    }
+}
 
 function addSelection(elementsList, category){
     elementsList.forEach(element => {
         element.addEventListener("change", (e)=>{
             const elementBox = document.querySelectorAll(`label[for=${e.target.id}] > div`)[0]
             elementBox.classList.add("selected")
-            validateButton(category)
+            validateButton(category, e.target.value)
 
             for(box of elementsList){
                 if(box.id !== e.target.id){
@@ -19,28 +68,58 @@ function addSelection(elementsList, category){
     })
 }
 
-addSelection(bebidas, "bebidas")
-addSelection(pratos, "pratos")
-addSelection(sobremesas, "sobremesas")
+addSelection(bebidas, "bebida")
+addSelection(pratos, "prato")
+addSelection(sobremesas, "sobremesa")
 
-function activeButton(){
-    const button = document.querySelector("button")
+
+function validateButton(category, value){
+    
+    categoriesSelected[`${category}`] = value;
+
+    if(categoriesSelected.prato && categoriesSelected.bebida && categoriesSelected.sobremesa){
+        const mensagem = window.encodeURIComponent(`Olá, gostaria de fazer o pedido:\n 
+    - Prato: ${menu.pratos[categoriesSelected.prato].name}\n
+    - Bebida: ${menu.bebidas[categoriesSelected.bebida].name}\n
+    - Sobremesa: ${menu.sobremesas[categoriesSelected.sobremesa].name}\n
+    Total: R$ ${(menu.pratos[categoriesSelected.prato].price + menu.bebidas[categoriesSelected.bebida].price + menu.sobremesas[categoriesSelected.sobremesa].price).toFixed(2)}`)
+        activeButton(mensagem)
+    }
+}
+
+function activeButton(mensagem){
+    const button = document.querySelector(".button button")
     button.classList.add("testButton")
     button.innerText="Fechar pedido"
 
-    button.addEventListener("click", () => {
 
+    button.addEventListener("click", () => {
+        activeModal(mensagem)
     })
 }
 
-function validateButton(item){
-    if(!itemsSelected.includes(item)){
-        itemsSelected.push(item)
-    }
+function activeModal(mensagem){
 
-    if(itemsSelected.length === 3){
-        activeButton()
-    }
+    const modal = document.querySelector(".modal")
+    const buttonOk = document.querySelectorAll(".modal button")[0]
+    const buttonCancel = document.querySelectorAll(".modal button")[1]
+    const total = document.querySelectorAll(".total p:last-child")[0]
+    const itensSelecionados = document.querySelectorAll(".modal .selectedItem")
+
+        itensSelecionados[0].innerHTML = `<p>${menu.pratos[categoriesSelected.prato].name}</p><p>${menu.pratos[categoriesSelected.prato].price}</p>`
+        itensSelecionados[1].innerHTML = `<p>${menu.bebidas[categoriesSelected.bebida].name}</p><p>${menu.bebidas[categoriesSelected.bebida].price}</p>`
+        itensSelecionados[2].innerHTML = `<p>${menu.sobremesas[categoriesSelected.sobremesa].name}</p><p>${menu.sobremesas[categoriesSelected.sobremesa].price}</p>`
+        total.innerHTML = `<p>${(menu.sobremesas[categoriesSelected.sobremesa].price+menu.bebidas[categoriesSelected.bebida].price+menu.pratos[categoriesSelected.prato].price).toFixed(2)}</p>`
+        modal.classList.add("activeModal")
+
+        buttonOk.addEventListener("click", ()=>{
+            window.open(`https://wa.me/5584991616256?text=${mensagem}`)
+            modal.classList.remove("activeModal")
+        })
+    
+        buttonCancel.addEventListener("click", () => {
+            modal.classList.remove("activeModal")
+        })
 }
 
 /*
