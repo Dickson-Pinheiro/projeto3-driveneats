9fc8fb414d71ff2ab3e2c650656a18d4c6a93a48
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2022-10-23 01:58:09 -0300

    corrigindo mensagem do pedido

--- index.js
+++ index.js
@@ -78,31 +78,25 @@ function validateButton(category, value){
     categoriesSelected[`${category}`] = value;
 
     if(categoriesSelected.prato && categoriesSelected.bebida && categoriesSelected.sobremesa){
-        const mensagem = window.encodeURIComponent(`Olá, gostaria de fazer o pedido:\n 
-    - Prato: ${menu.pratos[categoriesSelected.prato].name}\n
-    - Bebida: ${menu.bebidas[categoriesSelected.bebida].name}\n
-    - Sobremesa: ${menu.sobremesas[categoriesSelected.sobremesa].name}\n
-    Total: R$ ${(menu.pratos[categoriesSelected.prato].price + menu.bebidas[categoriesSelected.bebida].price + menu.sobremesas[categoriesSelected.sobremesa].price).toFixed(2)}`)
-        activeButton(mensagem)
+        activeButton()
     }
 }
 
-function activeButton(mensagem){
+function activeButton(){
     const button = document.querySelector(".button button")
     button.classList.add("testButton")
     button.innerText="Fechar pedido"
 
 
     button.addEventListener("click", () => {
-        activeModal(mensagem)
+        activeModal()
     })
 }
 
-function activeModal(mensagem){
+function activeModal(){
 
     const modal = document.querySelector(".modal")
-    const buttonOk = document.querySelectorAll(".modal button")[0]
-    const buttonCancel = document.querySelectorAll(".modal button")[1]
+    const [buttonOk, buttonCancel] = document.querySelectorAll(".modal button")
     const total = document.querySelectorAll(".total p:last-child")[0]
     const itensSelecionados = document.querySelectorAll(".modal .selectedItem")
 
@@ -110,10 +104,19 @@ function activeModal(mensagem){
         itensSelecionados[1].innerHTML = `<p>${menu.bebidas[categoriesSelected.bebida].name}</p><p>${menu.bebidas[categoriesSelected.bebida].price.toFixed(2)}</p>`
         itensSelecionados[2].innerHTML = `<p>${menu.sobremesas[categoriesSelected.sobremesa].name}</p><p>${menu.sobremesas[categoriesSelected.sobremesa].price.toFixed(2)}</p>`
         total.innerHTML = `<p>R&dollar; ${(menu.sobremesas[categoriesSelected.sobremesa].price+menu.bebidas[categoriesSelected.bebida].price+menu.pratos[categoriesSelected.prato].price).toFixed(2)}</p>`
-        modal.classList.add("activeModal")
 
-        buttonOk.addEventListener("click", ()=>{
-            window.open(`https://wa.me/5584991616256?text=${mensagem}`)
+
+        let mensagem = window.encodeURIComponent(`Olá, gostaria de fazer o pedido:\n 
+        - Prato: ${menu.pratos[categoriesSelected.prato].name}\n
+        - Bebida: ${menu.bebidas[categoriesSelected.bebida].name}\n
+        - Sobremesa: ${menu.sobremesas[categoriesSelected.sobremesa].name}\n
+        Total: R$ ${(menu.pratos[categoriesSelected.prato].price + menu.bebidas[categoriesSelected.bebida].price + menu.sobremesas[categoriesSelected.sobremesa].price).toFixed(2)}`)
+
+
+        modal.classList.add("activeModal")
+        buttonOk.setAttribute("value", mensagem)
+        buttonOk.addEventListener("click", (e)=>{
+            window.open(`https://wa.me/5584991616256?text=${e.target.value}`)
             modal.classList.remove("activeModal")
         })
     
@@ -121,111 +124,3 @@ function activeModal(mensagem){
             modal.classList.remove("activeModal")
         })
 }
-
-/*
-bebidas.forEach(bebida => {
-    bebida.addEventListener('change', (e) => {
-        const bebidaBox = document.querySelectorAll(`label[for=${e.target.id}] > div`)[0]
-        
-        switch(e.target.id){
-            case "bebida1":
-                document.querySelectorAll("label[for='bebida2'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='bebida3'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-
-                validateButton("bebida")
-
-                break;
-            case "bebida2":
-                document.querySelectorAll("label[for='bebida1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='bebida3'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-
-                validateButton("bebida")
-
-                break;
-            case "bebida3":
-                document.querySelectorAll("label[for='bebida1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='bebida2'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-
-                validateButton("bebida")
-
-                break;
-            default:
-            break;
-        }
-    })
-})
-
-pratos.forEach(prato => {
-    prato.addEventListener('change', (e) => {
-        const pratoBox = document.querySelectorAll(`label[for=${e.target.id}] > div`)[0]
-        
-        switch(e.target.id){
-            case "prato1":
-                document.querySelectorAll("label[for='prato2'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='prato3'] > div")[0].classList.remove("selected")
-                pratoBox.classList.add("selected")
-
-                validateButton("prato")
-
-                break;
-            case "prato2":
-                document.querySelectorAll("label[for='prato1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='prato3'] > div")[0].classList.remove("selected")
-                pratoBox.classList.add("selected")
-
-                validateButton("prato")
-
-                break;
-            case "prato3":
-                document.querySelectorAll("label[for='prato1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='prato2'] > div")[0].classList.remove("selected")
-                pratoBox.classList.add("selected")
-
-                validateButton("prato")
-
-                break;
-            default:
-            break;
-        }
-    })
-})
-
-sobremesas.forEach(sobremesa => {
-    sobremesa.addEventListener('change', (e) => {
-        const bebidaBox = document.querySelectorAll(`label[for=${e.target.id}] > div`)[0]
-        
-        switch(e.target.id){
-            case "sobremesa1":
-                document.querySelectorAll("label[for='sobremesa2'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='sobremesa3'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-                
-                validateButton("sobremesa")
-
-                break;
-            case "sobremesa2":
-                document.querySelectorAll("label[for='sobremesa1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='sobremesa3'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-
-                validateButton("sobremesa")
-
-                break;
-            case "sobremesa3":
-                document.querySelectorAll("label[for='sobremesa1'] > div")[0].classList.remove("selected")
-                document.querySelectorAll("label[for='sobremesa2'] > div")[0].classList.remove("selected")
-                bebidaBox.classList.add("selected")
-
-                validateButton("sobremesa")
-
-                break;
-            default:
-            break;
-        }
-    })
-})
-
-*/
